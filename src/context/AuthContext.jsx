e937d39a09374c4347a365927934060467da8515
fabek <fabek7577@gmail.com>
2024-01-29 07:35:01 +0500

    login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,10 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-  const [state, setState] = useState({logined: true});
+  const [state, setState] = useState({
+    username: "Farrux",
+    password: 12345678,
+  });
   return (
     <AuthContext.Provider value={{ state, setState }}>
       {children}
